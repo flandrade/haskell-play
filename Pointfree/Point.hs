ec4473e79618f9933f2ba35089b385ef722d9b55
Fernanda Andrade <fandrade@stackbuilders.com>
2018-02-16 14:02:04 -0500

    Add Pointfree example

---
+++ Pointfree/Point.hs
@@ -0,0 +1,20 @@
+module Point where
+
+getValue1 :: [Integer] -> Integer -> [Integer]
+getValue1 list value = tail (take 5 (map (value+) list))
+
+-- | Add comment referring to infix precedence. 
+getValue2 :: [Integer] -> Integer -> [Integer]
+getValue2 list value = tail $ take 5 $ map (value+) list
+
+getValue3 :: [Integer] -> Integer -> [Integer]
+getValue3 list value = tail . take 5 $ map (value+) list
+
+fx ::  Eq a => a -> [a] -> Int
+fx x list = length $ filter (== x) list
+
+fx2::  Eq a => a -> [a] -> Int
+fx2 x list = length (filter (== x) list)
+
+fx3 ::  Eq a => a -> [a] -> Int
+fx3 x = length . filter (== x)
