de6e014d49cb3ed4cd00213fef4d7f64ce9ef57e
Fernanda Andrade <fandrade@stackbuilders.com>
2018-04-11 10:38:54 -0500

    WIP

--- Functors/app/Main.hs
+++ Functors/app/Main.hs
@@ -1,6 +1,4 @@
 module Main where
 
-import FunctorInstances
-
 main :: IO ()
 main = putStrLn "Nothing here"
--- Monads/test/Spec.hs
+++ Monads/test/Spec.hs
@@ -1,2 +1,53 @@
+import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+import Test.QuickCheck.Test
+import MonadInstances
+import Data.Monoid
+
+
+-- Identity intance of Arbitrary and EqProp
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = do
+    a <- arbitrary
+    return $ Identity a
+
+instance Eq a => EqProp (Identity a) where
+  (=-=) = eq
+
+
+-- Contant instance of Arbitrary
+
+instance (Arbitrary a, Monoid a) => Arbitrary (Constant a b) where
+  arbitrary = do
+    a <- arbitrary
+    return $ Constant a
+
+instance Eq a => EqProp (Constant a b) where
+  (=-=) = eq
+
+
+-- List instance of Arbitrary
+
+instance (Arbitrary a) => Arbitrary (List a) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    frequency [ (1, return Nil),
+                (10, return (Cons x y)) ]
+
+instance Eq a => EqProp (List a) where
+  (=-=) = eq
+
+
+tests :: IO ()
+tests = hspec $ do
+  describe "Identity Applicative" $ do
+    it "satisfies property" $ do
+      quickBatch (monad $ Identity ("Test", [
+        ("hello" :: String) ], ("world" :: String)))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do tests
